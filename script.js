b3ebcfd168039480e46d8bbbd7db1a7951b67710
HassanElgallouchi <helgallouchi@gmail.com>
2025-04-19 16:38:09 +0200

    19-04-25_16:38:09

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const filmsPerPage = 20; // Nombre de films par page
+const filmsPerPage = 4; // Nombre de films par page
 let currentPage = 1;
 let totalPages = 1;
 let filmsData = [];
@@ -13,6 +13,43 @@ const fetchData = async () => {
   }
 };
 
+const showMoreData = (id) => {
+  const sectionMoreDataDom = document.getElementById("section-more-data");
+  const film = filmsData.find((film) => film.id === id);
+  let output = `
+  <div class="more-data">
+        <div id="close-more-data" onclick="closeMenuData()">X</div>
+        ${
+          film?.poster_src
+            ? `<img
+          src="${film?.poster_src}"
+          alt="Abner le chien magique"
+        />`
+            : ""
+        }
+        ${film?.title ? `<h3>${film?.title}</h3>` : ""}
+        ${film?.description ? `<p>Description : ${film.description}</p>` : ""}
+        ${film?.release_year ? `<p>Annee : ${film?.release_year}</p>` : ""}
+        ${film?.runtime ? `<p>Temps : ${film?.runtime}</p> ` : ""}
+        ${
+          film?.genres
+            ? `<p>Genres : ${film?.genres.map((genre) => genre)}</p>`
+            : ""
+        }
+        ${film?.audio ? `<p>Audio : ${film?.audio}</p>` : ""}
+        ${film?.quality ? `<p>Qualité : ${film?.quality}</p>` : ""}
+        ${film?.country ? `<p>Pays : ${film?.country}</p>` : ""}
+        ${film?.director ? `<p>Directeur : ${film?.director}</p>` : ""}
+        ${film?.actors.length > 1 ? `<p>Acteurs : ${film?.actors}</p>` : ""}
+      </div>
+  `;
+  sectionMoreDataDom.innerHTML = output;
+  sectionMoreDataDom.style.display = "flex";
+};
+const closeMenuData = () => {
+  const sectionMoreDataDom = document.getElementById("section-more-data");
+  sectionMoreDataDom.style.display = "none";
+};
 // Fonction pour afficher les films en fonction du genre et de la pagination
 const showData = () => {
   const dom = document.getElementById("list-films");
@@ -34,19 +71,19 @@ const showData = () => {
 
   let output = "";
   filmsToShow.forEach((film) => {
+    // console.log(film);
+    let genre = film.genres[0];
     output += `
       <div class="film">
+      <div class="badge-film"  onclick="showMoreData('${film?.id}')">
+        <img class="badge-img" src="/more.png" alt="menu" />
+      </div>
           <img class="poster_film" src="${film?.poster_src}" alt="" />
           <h2 class="titre_film">${film?.title}</h2>
           <div class="footer_film">
               <div class="duree_film">Duration: ${film?.runtime}</div>
               <div class="genre_film">
-                  Genre: ${film.genres
-                    .map(
-                      (genre) =>
-                        `<span class="genre-item" onclick="filterByGenre('${genre}')">${genre}</span>`
-                    )
-                    .join(", ")}
+                  Genre: <span class="genre-item" onclick="filterByGenre('${genre}')">${genre}</span>
               </div>
           </div>
           <a class="btn_film" href="${film?.iframe_src}">Watch</a>
@@ -62,25 +99,71 @@ const showData = () => {
 };
 
 // Fonction pour filtrer les films par genre
-// const filterByGenre = (genre) => {
-//   selectedGenre = genre;
-//   currentPage = 1; // Réinitialiser à la première page
-//   showData();
-// };
+const filterByGenre = (genre) => {
+  selectedGenre = genre;
+  currentPage = 1; // Réinitialiser à la première page
+  showData();
+};
 
 // Fonction pour réinitialiser le filtre
-// const resetFilter = () => {
-//   selectedGenre = null;
-//   currentPage = 1;
-//   showData();
-// };
+const resetFilter = () => {
+  selectedGenre = null;
+  currentPage = 1;
+  showData();
+};
+
+// search
+const searchData = () => {
+  const searchInput = document.getElementById("search-input");
+  const searchValue = searchInput.value.toLowerCase();
+  const filmsToShow = filmsData.filter((film) =>
+    film.title.toLowerCase().includes(searchValue)
+  );
+  totalPages = Math.ceil(filmsToShow.length / filmsPerPage);
+  if (currentPage > totalPages) currentPage = 1; // Réinitialiser si dépassement
+
+  const dom = document.getElementById("list-films");
+  if (!dom) return;
+
+  let output = "";
+  filmsToShow.forEach((film) => {
+    let genre = film.genres[0];
+    output += `
+      <div class="film">
+      <div class="badge-film" onclick="showMoreData('${film?.id}')">
+        <img class="badge-img" src="/more.png" alt="menu" />
+      </div>
+          <img class="poster_film" src="${film?.poster_src}" alt="" />
+          <h2 class="titre_film">${film?.title}</h2>
+          <div class="footer_film">
+              <div class="duree_film">Duration: ${film?.runtime}</div>
+              <div class="genre_film">
+                  Genre: <span class="genre-item" onclick="filterByGenre('${genre}')">${genre}</span>
+              </div>
+          </div>
+          <a class="btn_film" href="${film?.iframe_src}">Watch</a>
+      </div>
+    `;
+  });
+  dom.innerHTML = output;
+  updatePagination();
+
+  // 🔥 Scroll en haut après changement de page 🔥
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
 // Mettre à jour la pagination
 const updatePagination = () => {
   const paginationDom = document.getElementById("pagination");
   if (!paginationDom) return;
-
   paginationDom.innerHTML = `
+  <div class="home-btn" onclick="resetFilter()">
+  <img class="home-img" src="/home.png" alt="Home" />
+  </div>
+  <div id="search-bar">
+    <input type="text" id="search-input" placeholder="Rechercher un film..." />
+    <input type="button" id="search-button" value="Rechercher" onclick="searchData()" />
+  </div>
   <div class="pagination">
     <button class="prev" onclick="prevPage()" ${
       currentPage === 1 ? "disabled" : ""
@@ -89,12 +172,12 @@ const updatePagination = () => {
     <button class="next" onclick="nextPage()" ${
       currentPage === totalPages ? "disabled" : ""
     }>Suivant</button>
-    ${
-      selectedGenre
-        ? `<button onclick="resetFilter()">Réinitialiser</button>`
-        : ""
-    }
-  </div>`;
+    </div>`;
+  //  ${
+  //     selectedGenre
+  //       ? `<button onclick="resetFilter()" class="reset-filter">Réinitialiser</button>`
+  //       : ""
+  //   }
 };
 
 // Aller à la page précédente
